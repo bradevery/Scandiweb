27dc14376d1dc7506ed99df9e6d7b4f6ac40468e
bradevery <brahimlounas@gmail.com>
2026-03-14 11:46:32 +0100

    fix entrypoint - start apache without waiting for mysql

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-echo "Waiting for MySQL..."
-until mysql -h"$MYSQLHOST" -P"$MYSQLPORT" -u"$MYSQLUSER" -p"$MYSQLPASSWORD" "$MYSQLDATABASE" &>/dev/null; do
-  sleep 2
-done
-
-echo "Running schema..."
-mysql -h"$MYSQLHOST" -P"$MYSQLPORT" -u"$MYSQLUSER" -p"$MYSQLPASSWORD" "$MYSQLDATABASE" < /var/www/html/database/schema.sql
+echo "Importing schema..."
+mysql -h"$MYSQLHOST" -P"$MYSQLPORT" -u"$MYSQLUSER" -p"$MYSQLPASSWORD" "$MYSQLDATABASE" < /var/www/html/database/schema.sql 2>/dev/null || echo "Schema import skipped or already done."
 
 echo "Starting Apache..."
 apache2-foreground
