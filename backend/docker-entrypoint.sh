abd5edd259a50105d3ea283cd5ced5dd872fc5a3
bradevery <brahimlounas@gmail.com>
2026-03-14 11:59:10 +0100

    switch to nginx+php-fpm, fix apache mpm issue

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Importing schema..."
 mysql -h"$MYSQLHOST" -P"$MYSQLPORT" -u"$MYSQLUSER" -p"$MYSQLPASSWORD" "$MYSQLDATABASE" < /var/www/html/database/schema.sql 2>/dev/null || echo "Schema import skipped or already done."
 
-echo "Starting Apache..."
-apache2-foreground
+echo "Starting php-fpm..."
+php-fpm -D
+
+echo "Starting nginx..."
+nginx -g "daemon off;"
